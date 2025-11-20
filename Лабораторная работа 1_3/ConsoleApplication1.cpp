05d9f535789003ed11b7a5320dddad1ba2ad30c8
fallenthecreator <113187413+fallenthecreator@users.noreply.github.com>
2025-11-20 09:17:20 +0300

    Add files via upload

---
+++ Лабораторная работа 1_3/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <list>
+#include <iostream>
+
+int main()
+{
+    std::list<int> defaultlist{1,2,3,4,5,6};
+    for (int n : defaultlist)
+        if(n%2 == 0)
+        {
+            std::cout << n << '\t';
+        }
+    std::cout << std::endl;
+}
+
